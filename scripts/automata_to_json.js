321ab5ef502eeaf1d1e33ff695d78752629ddb6a
Sifus <sites+github@sifus.org>
2024-11-03 12:05:41 -0600

    Add automata to JSON converter
    A converter is needed to strip trailing commas

---
+++ scripts/automata_to_json.js
@@ -0,0 +1,7 @@
+import abs from "../js/automata/ab_star.js";
+import an_b2n from "../js/automata/an_b2n.js";
+import even from "../js/automata/even_zeroes.js";
+
+console.log(JSON.stringify(abs) + "\n");
+console.log(JSON.stringify(an_b2n) + "\n");
+console.log(JSON.stringify(even) + "\n");
